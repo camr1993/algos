5f30c9ba4a984d11ced7a5812d82c6bd04073e00
Cameron Ratliff <cratliff@colgate.edu>
2020-06-22 09:42:28 -0400

    max sums

--- Medium/2.12.maxSums.js
+++ Medium/2.12.maxSums.js
@@ -15,3 +15,23 @@ function maxSubsetSumNoAdjacent(array) {
   }
   return first
 }
+
+// Round 2:
+function maxSubsetSumNoAdjacent(array) {
+  if (!array.length) return 0
+  if (array.length === 1) return array[0]
+
+  // Basically keep track of the most recent two numbers
+  // as well as the max of them. Only use the max of the two
+  let second = array[0]
+  let first = Math.max(array[0], array[1])
+  for (let i = 2; i < array.length; i++) {
+    // should we keep the most recent num (first) or
+    // should we take the sum of two numbers back and
+    // the current num?
+    const current = Math.max(first, second + array[i])
+    second = first
+    first = current
+  }
+  return first
+}
