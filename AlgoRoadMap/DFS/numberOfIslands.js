a8db6f5d8035d1d3f9052b58eaf742786950e7df
Cameron Ratliff <cratliff@colgate.edu>
2020-07-01 11:06:37 -0400

    num of islands

---
+++ AlgoRoadMap/DFS/numberOfIslands.js
@@ -0,0 +1,29 @@
+var numIslands = function (grid) {
+  let count = 0
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        count++
+        dfs(grid, i, j)
+      }
+    }
+  }
+  return count
+}
+
+const dfs = (grid, i, j) => {
+  if (
+    i < 0 ||
+    j < 0 ||
+    i >= grid.length ||
+    j >= grid[i].length ||
+    grid[i][j] === '0'
+  )
+    return
+
+  grid[i][j] = '0'
+  dfs(grid, i + 1, j)
+  dfs(grid, i - 1, j)
+  dfs(grid, i, j + 1)
+  dfs(grid, i, j - 1)
+}
